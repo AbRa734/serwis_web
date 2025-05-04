616c11792bb69bd5f536f1581ca1e8c93fbb4547
Oliwier Szafrański <sz.o@yahoo.com>
2025-05-04 12:29:54 +0200

    Fix generowanie raportów, wysyłka email w ustawieniach

--- serwis_web/serwis_web/wwwroot/scripts.js
+++
@@ -1,6 +0,0 @@
-function downloadFileFromBase64(fileName, base64String, mimeType) {
-    const link = document.createElement("a");
-    link.href = `data:${mimeType};base64,${base64String}`;
-    link.download = fileName;
-    link.click();
-}
